2acdb4074abab34306c59766b6da10b06159bfab
Ivan Fernández Guinde <daw2al07@daw2-53.dip.local>
2026-09-22 13:38:30 +0200

    ejercicios operadores

--- PHP/ud3-26-27/www/app/Views/templates/left-menu.view.php
+++ PHP/ud3-26-27/www/app/Views/templates/left-menu.view.php
@@ -28,6 +28,41 @@
               </li>              
             </ul>
           </li>
+            <li class="nav-item menu-open">
+                <a href="#" class="nav-link">
+                    <i class="fas fa-biohazard"></i>
+                    <p>
+                        Operadores
+                        <i class="right fas fa-angle-left"></i>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                        <a href="ejercicio1-operadores" class="nav-link">
+                            <i class="fas fa-biohazard"></i>
+                            <p>OPERADORES 1</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="ejercicio2-operadores" class="nav-link">
+                            <i class="fas fa-biohazard"></i>
+                            <p>OPERADORES 1</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="ejercicio3-operadores" class="nav-link">
+                            <i class="fas fa-biohazard"></i>
+                            <p>OPERADORES 1</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="ejercicio4-operadores" class="nav-link">
+                            <i class="fas fa-biohazard"></i>
+                            <p>OPERADORES 1</p>
+                        </a>
+                    </li>
+                </ul>
+            </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
